2fc2496a5f6d106d85f52f1d421f2c007e92a218
Guillermo Ballesteros <132414127+guille2343@users.noreply.github.com>
2025-04-07 18:24:38 -0500

    wapo

--- src/comp/Home/Home.js
+++ src/comp/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -13,6 +14,58 @@ const Home = () => {
           <Link to="/shop" className="link">
             Shop Now
           </Link>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="container">
+          <div className="column">
+            <h3>Your Account</h3>
+            <ul>
+              <li>About Us</li>
+              <li>Account</li>
+              <li>Payment</li>
+            </ul>
+          </div>
+          <div className="column">
+            <h3>Products</h3>
+            <ul>
+              <li>Delivery</li>
+              <li>Track Order</li>
+              <li>New Product</li>
+            </ul>
+          </div>
+          <div className="column">
+            <h3>Contact Us</h3>
+            <ul>
+              <li>
+                <a
+                  href="https://www.facebook.com/share/16GWCuJukq/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaFacebook /> Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/flee_ked?igsh=MWowcXkzbWRxdzlnYw=="
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaInstagram /> Instagram
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.tiktok.com/@fleeked6?_t=ZS-8vLVdgso5sF&_r=1"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaTiktok /> TikTok
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- src/comp/Rout/Rout.js
+++ src/comp/Rout/Rout.js
@@ -1,9 +1,23 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "../Home/Home";
 import Shop from "../../Shop/Shop";
 
 const Rout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [location]);
+
   return (
     <>
       <Routes>
