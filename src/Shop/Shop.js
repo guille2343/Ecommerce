7a19f2e36be0a241379d4b95a55411f9ba0e3b38
Guillermo Ballesteros <132414127+guille2343@users.noreply.github.com>
2025-04-07 19:32:00 -0500

    mas cambios

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 import "./Shop.css";
 import HomeProducts from "../comp/HomeProducts/HomeProducts";
-import { FaEye } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa";
+import { FaEye, FaHeart } from "react-icons/fa";
+import Footer from "../comp/Footer/Footer";
 
 const Shop = () => {
   const [tredingProduct, setTredingProduct] = useState(HomeProducts);
 
   const filtercate = (x) => {
-    const filterProducto = HomeProducts.filter((curElm) => {
-      return curElm.type === x;
-    });
+    const filterProducto = HomeProducts.filter((curElm) => curElm.type === x);
     setTredingProduct(filterProducto);
   };
 
@@ -20,10 +18,13 @@ const Shop = () => {
 
   return (
     <div className="shop">
-      <h2># Shop</h2>
-      <p>Home - Shop</p>
       <div className="container">
         <div className="left-box">
+          <div className="shop-title">
+            <h2># Shop</h2>
+            <p>Home - Shop</p>
+          </div>
+
           <div className="category">
             <div className="header">
               <h3>All Categories</h3>
@@ -40,46 +41,49 @@ const Shop = () => {
               </ul>
             </div>
           </div>
-        </div>
-        <div className="right-box">
-          <div className="header">
-            <div className="heading">
-              <h2 onClick={() => allTredingProduct()}>Trending Product</h2>
-            </div>
-            <div className="cate">
-              <h3 onClick={() => filtercate("style")}>New</h3>
-              <h3 onClick={() => filtercate("top")}>Style</h3>
-              <h3 onClick={() => filtercate("new")}>Top Selling</h3>
+
+          {/* Nueva sección de filtros */}
+          <hr className="divider" />
+          <div className="left-box-filter">
+            <div className="box">
+              <ul>
+                <li onClick={() => allTredingProduct()}># Trending Product</li>
+                <li onClick={() => filtercate("new")}># New</li>
+                <li onClick={() => filtercate("style")}># Style</li>
+                <li onClick={() => filtercate("top")}># Top Selling</li>
+              </ul>
             </div>
           </div>
+        </div>
+
+        <div className="right-box">
           <div className="products">
             <div className="container">
-              {tredingProduct.map((curElm) => {
-                return (
-                  <div className="box" key={curElm.id}>
-                    <div className="img-box">
-                      <img src={curElm.imgen} alt="image" />
-                      <div className="icon">
-                        <div className="icon-box">
-                          <FaEye />
-                        </div>
-                        <div className="icon-box">
-                          <FaHeart />
-                        </div>
+              {tredingProduct.map((curElm) => (
+                <div className="box" key={curElm.id}>
+                  <div className="img-box">
+                    <img src={curElm.imgen} alt="product" />
+                    <div className="icon">
+                      <div className="icon-box">
+                        <FaEye />
+                      </div>
+                      <div className="icon-box">
+                        <FaHeart />
                       </div>
                     </div>
-                    <div className="info">
-                      <h3>{curElm.Name}</h3>
-                      <p>${curElm.Price}</p>
-                      <button className="btn">Add To Cart</button>
-                    </div>
                   </div>
-                );
-              })}
+                  <div className="info">
+                    <h3>{curElm.Name}</h3>
+                    <p>${curElm.Price}</p>
+                    <button className="btn">Add To Cart</button>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
--- src/comp/Home/Home.js
+++ src/comp/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
@@ -16,58 +16,7 @@ const Home = () => {
           </Link>
         </div>
       </div>
-      <div className="footer">
-        <div className="container">
-          <div className="column">
-            <h3>Your Account</h3>
-            <ul>
-              <li>About Us</li>
-              <li>Account</li>
-              <li>Payment</li>
-            </ul>
-          </div>
-          <div className="column">
-            <h3>Products</h3>
-            <ul>
-              <li>Delivery</li>
-              <li>Track Order</li>
-              <li>New Product</li>
-            </ul>
-          </div>
-          <div className="column">
-            <h3>Contact Us</h3>
-            <ul>
-              <li>
-                <a
-                  href="https://www.facebook.com/share/16GWCuJukq/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaFacebook /> Facebook
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.instagram.com/flee_ked?igsh=MWowcXkzbWRxdzlnYw=="
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaInstagram /> Instagram
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.tiktok.com/@fleeked6?_t=ZS-8vLVdgso5sF&_r=1"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaTiktok /> TikTok
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 };
